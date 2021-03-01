8edbda60499d4b41060a0b335b354fb311a4042f
SergeyL-1979 <65560756+SergeyL-1979@users.noreply.github.com>
2021-03-01 21:29:40 +0300

    Add files via upload

---
+++ Задание 1.8.1/pet.py
@@ -0,0 +1,11 @@
+from cat import Cat
+
+cats = Cat('Baron', 'boy', 2)
+print(cats.gender)
+print(cats.name)
+print(cats.get_say())
+
+cats = Cat('Sam', 'boy', 2)
+print(cats.gender)
+print(cats.name)
+print(cats.get_say())
